53c37f425f8164db0584ea6e7a72be779ed8ec5c
Chitransh-C <chitransh33333@gmail.com>
2025-03-25 17:23:22 +0530

    Updated files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-    apiKey: import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: import.meta.env.NEXT_PUBLIC_API,
     authDomain: import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: import.meta.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
